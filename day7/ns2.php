f029d7494c356c22609e17619486e5fffacdd7eb
Michael Kimsal <Michael Kimsal>
2010-12-14 11:03:58 -0500

    cleaning up demo

--- day7/ns2.php
+++ day7/ns2.php
@@ -2,18 +2,12 @@
 
 function __autoload($f) {
 	$t = str_replace("\\","/",$f);
-//	echo "including $t.php";
   include($t.".php");
 }
 
-//$name = new User();
-
-//include("ns.php");
 ?>
-<div id="33">
 <?php 
 use com\kimsal as lib;
 
 lib\foo::showFoo("my name is mike");
 ?>
-</div>
